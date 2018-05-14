bfc1b11e036ebe24dc8f61eb8a7886ee70550efc
Ettap <nymapat@gmail.com>
2018-05-14 23:15:51 +0300

    signup
    working version of the signup page with new database

--- sign_up_page.php
+++ sign_up_page.php
@@ -3,6 +3,7 @@ include("functions.php");
 ?>
 
 <!DOCTYPE html>
+
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -18,7 +19,7 @@ include("functions.php");
 	<body>
 		<div id="signup" class="border-signup col-6">
 			<fieldset>
-				<form class="signup" method="post">
+				<form class="signup" method="post" onsubmit="validateUser()">
 					<label for="username">Username</label></br>
 						<input type="name" name="username" placeholder="Username" required></br>
 						<input type="radio" name="gender1" value="1" class="gender"> Male<br>
@@ -31,7 +32,7 @@ include("functions.php");
 						<input type="password" name="passwordS" placeholder="password" required></br>
 						<input type="password" name="confirm_passwordS" placeholder="password again" required></br>
 					<label for="age">Age</label></br>
-						<input type="number" name"age" placeholder="age" required></br>
+						<input type="number" name="Uage" placeholder="age" required></br>
 					<label for="income">Income</label></br>&nbsp;
 						<input type="number" name="income" placeholder="income/year" required ></br>
 					<label for="currency">Currency</label></br>
@@ -40,63 +41,42 @@ include("functions.php");
 						 <input type="radio" name="gender2" value="1" class="gender"> Male</br>
 						 <input type="radio" name="gender2" value="2" class="gender"> Female</br>
 						 <input type="radio" name="gender2" value="3" class="gender"> both</br>
+                    <input type="submit" name="insert_new" value="Signup!">
+            <script>      
+                function validateUser(){
+                    console.log("button pressed");
+                    var password = document.getElementById("passwordS") , 
+                    confirm_password = document.getElementById("confirm_passwordS"),
+                    username = document.getElementById("username");
+                        console.log("checking user");
+                        if(username.value = <?php mysqli_query($link ,'SELECT FROM users WHERE username') ;?>){
+                            console.log("username is taken");
+                            username.setCustomValidity("Username is taken!");
+                           }else{
+                               console.log("user ok");
+                               username.setCustomValidity('');
+                               console.log("checking passwords");
+                            if(password.value != confirm_password.value) {
+                                confirm_password.setCustomValidity("Passwords Don't Match");
+                                console.log("passwords dont match");
+                                  } else {
+                                    confirm_password.setCustomValidity('');
+                                    console.log("password ok. sending to server");
+                                      $.ajax({
+                                            url: "functions.php",
+                                            data: "insert_new",
+                                        });
+                                  }
+                           }
 
+                }
 
-                    <input type="submit" name="insert" value="Signup" onClick="validatePassword">
+                password.onchange = validateUser();
+                confirm_password.onkeyup = validateUser();
+            </script>
 				</form>
 			</fieldset>
-<script>
-	var password = document.getElementById("passwordS") , 
-	confirm_password = document.getElementById("confirm_passwordS");
-	function checkUser(){
-
-		if(username.value = <?php mysqli_query('SELECT FROM users WHERE $username') ;?>){
-		   username.setCustomValidity("Username is taken!");
-		   }else{
-		   username.setCustomValidity('');   
-		   }
-	}
-	function validatePassword(){
-	  if(passwordS.value != confirm_password.value) {
-		confirm_password.setCustomValidity("Passwords Don't Match");
-	  } else {
-		confirm_password.setCustomValidity('');
-		  $.ajax( { })
-	  }
-	}
-
-	password.onchange = validatePassword;
-	confirm_password.onkeyup = validatePassword;
-</script>
-<?php
-			if(isset($_POST['insert_new'])){
-				$username = $_POST['username'];
-				$email = $_POST['email'];
-				$password1 = password_hash($_POST['passwordS'], PASSWORD_DEFAULT);
-				$postnum = $_POST['postnum'];
-				$gender = $_POST['gender1.value'];
-				$seeking = $_POST['gender2.value'];
-				$income = $_POST['income'];
-				$age = $_POST['age'];
-				
-				
-			$q_user_insert= "INSERT INTO `datingsite`.`users` 
-			(`userID`, `username`, `email`, `password` ,´postnum´ , `pay`, `currency`, `seeking`
-			, `gender` , `age`)
-			 VALUES 
-			(NULL, '$username', '$email', '$password1', '$postnum' ,'$income' , 'NULL' , '$seeking' 
-			,'$gender', '$age')";
-						 echo $q_user_insert;
-			$r_user_insert = mysqli_query($link, $q_user_insert);
-
-				if(!$r_user_insert){
-					echo "<p>The insert failed <br>" .
-					mysqli_error($link). "</p>";
-				} else {
-					echo "<p>A new user has ben added!</p>";	
-				}
-			}
-		?>
 		</div>
 	</body>
+            
 </html>
