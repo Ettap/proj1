f0bc33a9ef39477095a662ccb0cbe2b0baa4e175
Ettap <nymapat@gmail.com>
2018-05-22 21:22:19 +0300

    1

--- info.php
+++ info.php
@@ -25,12 +25,8 @@ include("functions.php");
   	      <input  type="submit" name="Seek" value="Search">
   </form>
 
-
 <?php
 
-$q_users = "SELECT * FROM users WHERE userID";
-$r_people = mysqli_query($link, $q_users);
-$results_per_page = 20;
 
 echo "<p><a href='index.php' title='main'>Main</a></p>";
 
@@ -53,88 +49,9 @@ $these_people = mysqli_query($link, $select_users);
   ?>
   <p><a href="info.php?id=<?php echo $rowii['userID']+1;?>" title="info">Next</a></p>
   <p><a href="info.php?id=<?php echo $rowii['userID']-1;?>" title="info">Previous</a></p>
-  <?php
-}
 
-//idea: if specific id is not set, display several profiles
-elseif(!isset($_GET['id']) && !isset($_GET['page'])){
-  foreach(['id'] as $valueidk){
-    echo "<p>Neither ID nor Page is set! Displaying all:</p>";
-
-    while($rowi = mysqli_fetch_array($r_people))
-    {
-      echo $rowi['username'];
-      ?>
-      <p><a href="info.php?id=<?php echo $rowi['userID'];?>" title="info">Read more</a></p>
-      <?php
-
-}
-
-}}
-
-
-
-if (isset($_GET["page"])) {
-$page  = $_GET["page"];
-//if (!isset($_GET["page"]) && !isset($_GET["id"])) {
-//$page=1;
-
-
-
-$start_from = ($page-1) * $results_per_page;
-$sqlyyy = "SELECT * FROM users LIMIT $results_per_page OFFSET $start_from";
-$rs_result = $link->query($sqlyyy);
-
-$sqlooo = "SELECT COUNT(userID) AS total FROM users";
-$result = $link->query($sqlooo);
-$rowl = $result->fetch_assoc();
-$total_pages = ceil($rowl["total"] / $results_per_page);
-
-?>
-<table border="1" cellpadding="4">
-<tr>
-    <td bgcolor="#CCCCCC"><strong>Name</strong></td>
-    <td bgcolor="#CCCCCC"><strong>Description</strong></td>
-    <td bgcolor="#CCCCCC"><strong>Age</strong></td>
-    <td bgcolor="#CCCCCC"><strong>Email</strong></td>
-    <td bgcolor="#CCCCCC"><strong>Income</strong></td>
-    <td bgcolor="#CCCCCC"><strong>Profile</strong></td>
-</tr>
 <?php
- while($row = $rs_result->fetch_assoc()) {
- //while($rowi = query($rs_result)) {
-?>
-            <tr>
-
-            <td><?php echo $row["username"]; ?></td>
-            <td><?php echo $row["description"]; ?></td>
-            <td><?php echo $row["age"]; ?></td>
-            <td><?php echo $row["email"]; ?></td>
-            <td><?php echo $row["income"]; ?></td>
-            <td><a href='info.php?id=<?php echo $row['userID'];?>' title='info'>Link</a></td>
-            </tr>
-<?php
-
 }
-//gives the table of users a page system
-$sqlooo = "SELECT COUNT(userID) AS total FROM users";
-$result = $link->query($sqlooo);
-$rowl = $result->fetch_assoc();
-$total_pages = ceil($rowl["total"] / $results_per_page);
-echo "<p>Page: </p>";
-for ($i=1; $i<=$total_pages; $i++) {
-   echo "<a href='info.php?page=".$i."'>".$i.', '."</a> ";
-};
-};
-?>
-</table>
-<?php
-
-
-
-
-
-
 //this seems to be the wrong way to do this...
 //PLAN: link userID to info.php?id="userID",
 //$thispagenumber = $rowi['userID'];
@@ -153,12 +70,6 @@ for ($i=1; $i<=$total_pages; $i++) {
 ?>
 
 </div>
-<script>
-
-</script>
-
-
-
 
   </body>
   </html>
