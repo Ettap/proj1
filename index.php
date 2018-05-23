9155839c7147ae3b9d22b77222081b917969fff1
Ettap <nymapat@gmail.com>
2018-05-23 12:37:51 +0300

    reverts back to "last"

--- index.php
+++ index.php
@@ -1,111 +1,70 @@
 <?php
-include( "functions.php" );
-include( 'db_connect.php' );
-session_start();
+include("functions.php");
+include('db_connect.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
+  <head>
+    <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Dating site Project1</title>
-	<!--Desing css all changes made in here -->
+    <title>Dating site Project1</title>
+	 <!--Desing css all changes made in here -->
 	<link href="css/design.css" rel="stylesheet">
-	<!-- Bootstrap ignore this -->
+    <!-- Bootstrap ignore this -->
 	<link href="css/bootstrap-4.0.0.css" rel="stylesheet">
 
-</head>
+  </head>
+  <body>
+	  <header></header>
 
-<body>
-	<header></header>
-	<div id="login" class="border-signup col-6">
-		<fieldset>
-
-
-
-			<?php
-			if ( !isset( $_SESSION[ 'user' ] ) ) {
-				?>
-			<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-				<fieldset>
-					<legend>Login</legend>
-					<label for="username">Username</label></br>
-					<input type="username" name="username" id="username" required></br>
-					<label for="password">Password</label></br>
-					<input type="password" name="password" id="password" required>
-
-					<input type="submit" name="login" value="Logga in!">
-				</fieldset>
-			</form>
-			<?php } else { ?>
-	
-			<fieldset>
-				<legend>Logout</legend>
-				<div class="button3">
-					<ul>
-						<li><a href="index.php?logout=yes">Logga ut</a>
-						</li>
-					</ul>
-				</div>
-			</fieldset>
-			<?php } ?>
-		</fieldset>
-		</form>
-	</div>
 	<div id="content">
-		<h1>Find sexy singles in your area</h1>
-		<!-- body code goes here -->
-		<nav>
-			<a href="login_page.php">Login page</a><br>
-			<a href="sign_up_page.php">signup page WIP here</a><br>
-			<a href="info.php" title='info'>View list of all profiles here</a><br>
-			<a href='info.php?page=1' title='info'>View table of profiles (better ui)</a>
-		</nav>
-		<div class="col-3">
+    <h1>Find sexy singles in your area</h1>
+  	<!-- body code goes here -->
+	  <nav>
+      <a href="login_page.php">Login page</a><br>
+      <a href="sign_up_page.php">signup page WIP here</a><br>
+      <a href="info.php" title='info'>View list of all profiles here</a><br>
+      <a href='info.php?page=1' title='info'>View table of profiles (better ui)</a>
+	  </nav>
+  <div class="col-3">
 
-			<?php
+    <?php
 
-			if ( isset( $_GET[ 'login' ] ) ) {
-				include( "login_page.php" );
-			} else {
-				?>
+            if(isset($_GET['login'])){
+              include ("login_page.php");
+            } else {
+    ?>
 
-			<fieldset>
-
-				<div id="listingsAll">
-					<legend>Users</legend>
-					<?php
+		<fieldset>
+            
+			<div id="listingsAll" class="col-12">
+                <legend>Users</legend>
+				<?php
 					$q_users = "SELECT * FROM users";
 
-					$r_people = mysqli_query( $link, $q_users );
+					$r_people = mysqli_query($link, $q_users);
 
-					while ( $row = mysqli_fetch_array( $r_people ) ) {
-						?>
-					<div class="listings col-5">
-						<h4>
-							<?php echo $row['username'];?> </h4>
-						<p>
-							<?php echo $row['description']; ?>
-						</p>
-						<p><a href="info.php?id=<?php echo $row['userID'];?>" title="info">Read more</a>
-						</p>
+					while($row = mysqli_fetch_array($r_people)) {
+				?>
+                <div class="listings col-5">
+					<h4><?php echo $row['username'];?> </h4>
+					<p><a href="info.php?id=<?php echo $row['userID'];?>" title="info">Read more</a></p>
 
-					</div>
-					<?php
-					}
-					};
-					?>
+                </div>
+				<?php
+      }};
+				?>
 
-				</div>
-			</fieldset>
-		</div>
+			</div>
+		</fieldset>
 	</div>
-	<footer></footer>
+	</div>
+	  <footer></footer>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<!-- Include all compiled plugins (below), or include individual files as needed -->
 	<script src="js/popper.min.js"></script>
 	<script src="js/bootstrap-4.0.0.js"></script>
-</body>
+  </body>
 </html>
--- sign_up_page.php
+++ sign_up_page.php
@@ -14,8 +14,9 @@ include("functions.php");
 	<link href="css/design.css" rel="stylesheet"></link>
     <!-- Bootstrap ignore this -->
 	<link href="css/bootstrap-4.0.0.css" rel="stylesheet"></link>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<!--<link href="https://openexchangerates.org/api/latest.json?app_id=3a43b77ceddc4e609181d29cc0ce35f9"></link>-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/jquery-3.3.1.min.js"></script>
+	<!--<link href="https://openexchangerates.org/api/latest.json?app_id=3a43b77ceddc4e609181d29cc0ce35f9"></link>
+-->
 
 </head>
 
@@ -34,15 +35,15 @@ $.get('https://openexchangerates.org/api/latest.json', {app_id: '3a43b77ceddc4e6
 						<input type="radio" name="gender1" value="1" class="gender"> Male<br>
 						<input type="radio" name="gender1" value="2" class="gender"> Female<br>
 					<label for="email">Email</label></br>
-						<input type="email" name="email" placeholder="email@example.com" required ></br>
+						<input type="email" name="email" placeholder="email@example.com" ></br>
 						<label for="postalcode">Postalcode</label></br>
-						<input type="text" name="postnum" placeholder="postalcode"required ></br>
+						<input type="number" name="postnum" placeholder="postalcode" ></br>
 					<label for="password">Password</label></br>
-						<input type="password" name="passwordS" placeholder="password" required></br>
-						<input type="password" name="confirm_passwordS" placeholder="password again" required ></br>
+						<input type="password" name="passwordS" placeholder="password" ></br>
+						<input type="password" name="confirm_passwordS" placeholder="password again" ></br>
 					<label for="age">Age</label></br>
 						<input type="number" name="Uage" placeholder="age" ></br>
-					<label for="income">Income</label></br>
+					<label for="income">Income</label></br>&nbsp;
 						<input type="number" name="income" placeholder="income/year" ></br>
 					<label for="currency">Currency</label></br>
 					<select name="currency" onChange="optionSelect()">
@@ -227,14 +228,9 @@ $.get('https://openexchangerates.org/api/latest.json', {app_id: '3a43b77ceddc4e6
 			 document.getElementById("insertNew").addEventListener("click" ,function(){
 																   validateUser();
 																   });
-				function validateUser(){
-					$.ajax({
-				url: 'functions.php',
-				type: 'post',
-				data: 'insert_new',
-				}
-						   );
-				}
+    function validateUser{
+        
+    }
             </script>
 
 		</div>
